bf5ecbea7fb335a015b0648da334d68696745ea2
talbor_ <194424255+RaiusTalbor@users.noreply.github.com>
2025-03-16 22:07:19 +0100

    Importieren.py Update
    Fahrer, Fahrzeuge, Strecken können jetzt importiert werden;

--- Importieren.py
+++ Importieren.py
@@ -7,18 +7,11 @@ import random
 import time
 from tkinter import *
 from tkinter import filedialog
-import MebeV1 as M1 #Mebe1-Integrierung
 import Daten    #Lesen, Schreiben von Dateien
-
-def abbruch():
-    #Abbruch des Importierens
-    a = a
 
 def Importieren():
     pfad = filedialog.askopenfilename()
-    f = open(pfad, mode = 'rb')
-    streaminhalt = pickle.load(f)
-    f.close()
+    streaminhalt = Daten.lesen(pfad)
 
     #testet gültiges Format
     #mit in? Problem: Liegt ja nur in Datenbank
@@ -27,17 +20,15 @@ def Importieren():
 
     #Meisterschaft selbst
     #meisterschaft = pfad.replace()
+    #TODO: Wie Meisterschaft selbst ins Verzeichnis importieren?
+    #Muss ja irgendwie die Datei validieren und den Namen extrahieren - aus askopenfilename!
 
     #----------Strecken
     #öffnet zu importierende Strecken
-    f = open(streaminhalt[0], mode='wb')
-    streckenliste = pickle.load(f)
-    f.close()
+    streckenliste = Daten.lesen(streaminhalt[0])
     
     #öffnen des vorhandenen Streckenverzeichnisses
-    f = open('Datenbank/Strecken/000 - Verzeichnis Strecken.dat', mode='rb')
-    streckenMebe2 = pickle.load(f)
-    f.close()
+    streckenMebe2 = Daten.lesen('Datenbank/Strecken/000 - Verzeichnis Strecken.dat')
 
     #hinzufügen der neuen Strecken
     for i in range(0, len(streckenliste)):
@@ -45,16 +36,49 @@ def Importieren():
             streckenMebe2.append(streckenliste[i])
 
     #speichern des Streckenverzeichnisses
-    f = open('Datenbank/Strecken/000 - Verzeichnis Strecken.dat', mode='wb')
-    pickle.dump(streckenMebe2, f)
-    f.close()
+    Daten.schreiben('Datenbank/Strecken/000 - Verzeichnis Strecken.dat', streckenMebe2)
 
     #----------Fahrer
+    fahrerliste = Daten.lesen(streaminhalt[1])
+    
+    #öffnen des vorhandenen Fahrerverzeichnisses
+    fahrerMebe2 = Daten.lesen('Datenbank/Strecken/000 - Verzeichnis Fahrer.dat')
 
-    #Fahrzeuge
+    #hinzufügen der neuen Fahrer
+    for i in range(0, len(fahrerliste)):
+        if  fahrerliste[i] not in fahrerMebe2:
+            fahrerMebe2.append(fahrerliste[i])
+
+    #speichern des Fahrerverzeichnisses
+    Daten.schreiben('Datenbank/Strecken/000 - Verzeichnis Fahrer.dat', fahrerMebe2)
+
+    #----------Fahrzeuge
     #durchsuchen jeder Fahrer nach fahrzeug, was evtl. nicht dabei ist
 
-    #Pfad
+    #hinzufügen der neuen Fahrzeuge, nur die Fahrer, die auch wirklich hinzukommen
+    for i in range(0, fahrerliste):
+
+        #baut den Pfad des jeweiligen Fahrers zusammen
+        pfadFahrer = "Datenbank/Fahrer/"
+        pfadFahrer += fahrerliste[i]
+        pfadFahrer += ".dat"
+
+        fahrerdaten = Daten.lesen(pfadFahrer)
+
+        #nur Fahrzeug herausgesucht
+        neuesFahrzeug = fahrerdaten[7]
+
+        #öffnen des vorhandenen Fahrzeugverzeichnisses
+        fahrzeugMebe2 = Daten.lesen('Datenbank/Strecken/000 - Verzeichnis Fahrzeuge.dat')
+
+        #hinzufügen des neuen Fahrzeugs
+        if  neuesFahrzeug not in fahrzeugMebe2:
+            fahrzeugMebe2.append(neuesFahrzeug)
+
+    #speichern des Fahrerverzeichnisses
+    Daten.schreiben('Datenbank/Strecken/000 - Verzeichnis Fahrzeuge.dat', fahrzeugMebe2)
+
+#----------View
 
 fensterImportieren = Tk()
 fensterImportieren.title("Importieren")
